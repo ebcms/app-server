ad59bc3b36b187d383b88ebe1051ef3843a55e97
ebcms <1540837821@qq.com>
2021-01-25 18:12:33 +0800

    1

--- src/library/Http/Download.php
+++ src/library/Http/Download.php
@@ -6,28 +6,28 @@ namespace App\Ebcms\Server\Http;
 
 use App\Ebcms\Admin\Http\Common;
 use App\Ebcms\Server\Traits\DirTrait;
-use Ebcms\App;
 use Ebcms\Session;
+use Throwable;
 
 class Download extends Common
 {
     use DirTrait;
 
     public function get(
-        App $app,
         Session $session
     ) {
         $upgrade = $session->get('upgrade');
         try {
             $content = file_get_contents($upgrade['source']);
-            if (md5($content) != $upgrade['md5']) {
-                return $this->failure('校验失败！');
-            }
-        } catch (\Throwable $th) {
+        } catch (Throwable $th) {
             return $this->failure('升级包下载失败，请稍后再试~');
         }
 
-        $tmpfile = $app->getAppPath() . '/runtime/upgrade.tmp';
+        if (md5($content) != $upgrade['md5']) {
+            return $this->failure('校验失败！');
+        }
+
+        $tmpfile = tempnam(sys_get_temp_dir(), 'serverupgrade');
 
         file_put_contents($tmpfile, $content);
 
